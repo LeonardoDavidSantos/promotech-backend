05302147748d4ab3e659f2a20377639be72ade9b
Vitor Hilário <vitorbarbosahilario@gmail.com>
2025-01-22 11:29:04 -0300

    fix: invalid allowed methods

--- src/main/java/com/promotech/api/infra/security/SecurityConfiguration.java
+++ src/main/java/com/promotech/api/infra/security/SecurityConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
@@ -27,11 +26,12 @@ public class SecurityConfiguration {
     private SecurityFilter securityFilter;
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public UrlBasedCorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost", "https://promotech-one.vercel.app"));
-        configuration.setAllowedHeaders(List.of("GET", "PUT", "DELETE", "POST"));
-        configuration.setAllowedHeaders(List.of("Authorization"));
+        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedMethods(List.of("GET", "PUT", "DELETE", "POST"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
@@ -40,7 +40,6 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .cors(c -> c.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
